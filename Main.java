64f9677411266cd5f361676a2364fecd5ef30d7b
Ayush <aayushsoni90@gmail.com>
2024-04-03 23:43:44 +0530

    Added new InterestCalculator

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Addition " + Calculator.add(10, 56));
+        System.out.println("Interest "+ InterestCalculator.calculate(10000000,9, 5));
     }
 }
