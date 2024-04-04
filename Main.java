8f63ec67118c69779ceb8b1be428f01be56660f2
Ayush <aayushsoni90@gmail.com>
2024-04-04 19:55:58 +0530

    Added a compound interest calculator.

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Addition " + Calculator.add(10, 56));
-        System.out.println("Interest "+ InterestCalculator.calculate(10000000,9, 5));
+        System.out.println("Compound Interest "+ CompoundInterestCalculator.calculate(10000000,9, 5));
+
     }
 }
