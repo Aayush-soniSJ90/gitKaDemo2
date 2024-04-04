5f3e198aa810cbd35364a77583fbccf89981c3ad
Ayush <aayushsoni90@gmail.com>
2024-04-04 14:57:40 +0530

    Added a SIP calculator.

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Addition " + Calculator.add(10, 56));
         System.out.println("Interest "+ InterestCalculator.calculate(10000000,9, 5));
+        System.out.println("SIP Return " + SIPCalculator.calculate(10000, 12, 12, 20));
     }
 }
